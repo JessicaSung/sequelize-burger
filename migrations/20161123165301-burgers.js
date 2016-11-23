496e3afd4a1d829ea72a65730bf68d10b26760fb
JessicaSung <jessica.sung@utexas.edu>
2016-11-23 11:08:35 -0600

    Created burgers migration and coded bulkCreate and destroy.

---
+++ migrations/20161123165301-burgers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+var models = require('../models');
+
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Altering commands here.
+    */
+    return models.Burgers.bulkCreate(
+      [
+        {burger_name: "Double Double"},
+        {burger_name: "Thick & Hearty"},
+        {burger_name: "Bacon Cheeseburger"},
+        {burger_name: "Whopper"},
+        {burger_name: "McDouble"}
+      ]
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Reverting commands here.      
+    */
+    return models.Burgers.destroy({where:{burger_name: [
+        "Double Double",
+        "Thick & Hearty",
+        "Bacon Cheeseburger",
+        "hopper",
+        "SMcDouble"
+    ]}})
+  }
+};
