23a6883b1a79236cd572aead8172d98646311e5e
JessicaSung <jessica.sung@utexas.edu>
2016-11-22 21:09:28 -0600

    Changed the \ to / in the index.js file because sequelize has a bug in the npm package

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\config\config.json')[env];
+// have to flip the / in the line below because there's an error with sequelize init on windows computers T_T
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
